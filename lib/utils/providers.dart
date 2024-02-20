a73a4b4a3010242697c5a0ea75bb8e3b5c835489
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-02-20 14:22:50 +0530

    Update providers.dart

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -22,3 +22,36 @@ class InputData with ChangeNotifier {
     notifyListeners();
   }
 }
+
+class Process with ChangeNotifier {
+  late List<List<TextEditingController>> controller = [];
+  List<String> id = [], arivalTime = [], burstTime = [];
+  void init(int n) {
+    controller.clear();
+    id.clear();
+    arivalTime.clear();
+    burstTime.clear();
+    for (int i = 1; i <= n; i++) {
+      id.add("P$i");
+      arivalTime.add('0');
+      burstTime.add('0');
+      List<TextEditingController> temp = [
+        TextEditingController(),
+        TextEditingController(),
+        TextEditingController(),
+      ];
+      controller.add(temp);
+      controller.last[0].text = "P$i";
+      controller.last[1].text = "0";
+      controller.last[2].text = "0";
+    }
+  }
+
+  TextEditingController getController(int i, int j) {
+    return controller[i][j];
+  }
+
+  void getValue(int i, int j) {
+    print(controller[i][j].text);
+  }
+}
