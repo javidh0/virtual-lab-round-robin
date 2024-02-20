3814d8fee75d55827c24a04f20b1141499086871
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-02-20 12:21:35 +0530

    Update execute_page.dart

--- lib/execute_page.dart
+++ lib/execute_page.dart
@@ -78,8 +78,10 @@ class RoundRobinExecuteWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: getWidth(100, context),
+      height: getHeight(80, context),
       child: const Column(
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           ProcessesDisplay(),
           Table(),
@@ -89,20 +91,75 @@ class RoundRobinExecuteWidget extends StatelessWidget {
   }
 }
 
-class ProcessesDisplay extends StatelessWidget {
+class ProcessesDisplay extends StatefulWidget {
   const ProcessesDisplay({super.key});
 
   @override
+  State<ProcessesDisplay> createState() => _ProcessesDisplayState();
+}
+
+class _ProcessesDisplayState extends State<ProcessesDisplay>
+    with SingleTickerProviderStateMixin {
+  double opacity = 0;
+  late AnimationController controler;
+  late CurvedAnimation animator;
+  @override
+  void initState() {
+    controler = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+      reverseDuration: const Duration(seconds: 1),
+    );
+    animator = CurvedAnimation(parent: controler, curve: Curves.bounceInOut);
+    super.initState();
+    animator.addListener(() {
+      setState(() {
+        opacity = animator.value;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        ProcessBox(),
-        ProcessBox(),
-        ProcessBox(),
-        ProcessBox(),
-        ProcessBox(),
-        ProcessBox(),
+        ElevatedButton(
+            onPressed: () {
+              controler.forward();
+            },
+            child: const Text("Next")),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ProcessBox(opacity: 1 - opacity),
+            ProcessBox(),
+            ProcessBox(),
+            ProcessBox(),
+            ProcessBox(),
+            ProcessBox(),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              width: getWidth(8, context),
+              height: getHeight(20, context),
+              color: Colors.amber,
+              child: Padding(
+                padding: EdgeInsets.only(top: getHeight(3, context)),
+                child: ProcessBox(
+                  opacity: opacity,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text("CPU", style: text_1),
+            )
+          ],
+        )
       ],
     );
   }
@@ -111,32 +168,51 @@ class ProcessesDisplay extends StatelessWidget {
 class ProcessBox extends StatelessWidget {
   const ProcessBox({
     super.key,
+    this.opacity = 1,
   });
+
+  final double opacity;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color.fromRGBO(135, 107, 170, 1),
-          border: Border.all(
-            color: Colors.black,
-            width: 1,
-          ),
-        ),
-        height: getHeight(10, context),
-        width: getHeight(10, context),
-        child: Center(
-          child: Text(
-            "10",
-            style: text_1.copyWith(
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
-              color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(135, 107, 170, opacity),
+              border: Border.all(
+                color: Color.fromRGBO(0, 0, 0, opacity),
+                width: 1,
+              ),
+            ),
+            height: getHeight(10, context),
+            width: getHeight(10, context),
+            child: Center(
+              child: Text(
+                "10",
+                style: text_1.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color.fromRGBO(255, 255, 255, opacity),
+                ),
+              ),
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "P1",
+              style: text_1.copyWith(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                color: Color.fromRGBO(0, 0, 0, opacity),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
